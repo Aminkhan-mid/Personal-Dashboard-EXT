e7a7045aed4149d74114717a38f9be5c81691244
Amin Khan <154885880+Aminkhan-mid@users.noreply.github.com>
2025-09-26 17:57:57 +0530

    made a database of pokemons and divedied them in colors and types.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const weather = document.getElementById("weather")
 const time = document.getElementById("time")
 const quote = document.getElementById("quote")
 const pokemon = document.getElementById("pokemon")
+const allPokemons = document.getElementById("allPokemonBtn")
+
 
 
 
@@ -13,6 +15,11 @@ navigator.geolocation.getCurrentPosition(async position => {
   const longitude = crd.longitude;
   const apiKey = "1ad29d4803cb3b86c237422b71268c9d";
 
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+    const data = await res.json()
+    const buttonGif = data.sprites.versions["generation-v"]["black-white"].animated.back_default  
+    console.log(buttonGif)
+    console.log("hello")
   try{
       const res = await fetch(
           `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`);
@@ -22,12 +29,14 @@ navigator.geolocation.getCurrentPosition(async position => {
           const data = await res.json();
           const iconID = data.weather[0].icon
           const weatherIcon = `https://openweathermap.org/img/wn/${iconID}@2x.png`
-          weather.innerHTML = 
-          `
+          weather.innerHTML = `
         <div class="weather-child">
             <img class="weather-icon" src="${weatherIcon}" alt="${data.weather[0].description}"/>
             <p class="city-name">${data.name}</p>
-        </div>
+            </div>
+          `
+          allPokemons.innerHTML = `
+                <img src="${buttonGif}" alt="pikachu">
           `
         }
         catch(err){
